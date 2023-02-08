1e945dbd3c38cc588dbe47d2a3e98817903d8dcc
Ivan Jurado <ijurado@MacBook-Pro-de-dev.local>
2023-02-08 12:02:59 -0600

    versioning

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@
  */
 
  define( 'MG_TEMPLATES_PATH', get_stylesheet_directory() . '/templates//' );
+ define( 'MG_THEME_VERSION', '0.0.1' );
 
 /**
  * Load child theme css and optional scripts
@@ -14,12 +15,19 @@
  */
 function hello_elementor_child_enqueue_scripts() {
 	wp_enqueue_style(
+		'default',
+		get_stylesheet_directory_uri() . '/default.css',
+		[],
+		MG_THEME_VERSION,
+	);
+
+	wp_enqueue_style(
 		'muguerza-theme',
 		get_stylesheet_directory_uri() . '/style.css',
 		[
 			'hello-elementor-theme-style',
 		],
-		'0.0.1'
+		MG_THEME_VERSION,
 	);
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
