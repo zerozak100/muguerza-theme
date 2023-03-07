c58a17a7430eaa143d32f4f22e5c07b84e4e35df
Ivan Jurado <ijurado@MacBook-Pro-de-dev.local>
2023-03-07 17:32:36 -0600

    obtener nombre de la ubicación guardada

--- inc/mg-helpers.php
+++ inc/mg-helpers.php
@@ -4,6 +4,22 @@ function mg_get_template( $template_name, $args = array() ) {
 	wc_get_template( $template_name, $args, '', MG_TEMPLATES_PATH );
 }
 
+function mg_get_saved_location_name() {
+	$user = MG_User::current();
+
+	$location_id = $user->get_location();
+
+	$location = MG_Location::get_location( $location_id );
+
+	$name = '';
+
+	if ( $location ) {
+		$name = $location[ 'name' ];
+	}
+
+	return $name;
+}
+
 function d( $var ) {
 	echo "<pre>";
 	var_dump( $var );
