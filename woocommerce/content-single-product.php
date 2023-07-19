1ab45b21df8c832396260e339e1dfc7ee44b9211
Jeus Jurado <ijurado@acsyt.com>
2023-07-19 13:12:28 -0600

    mejora plantilla de producto

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -33,8 +33,14 @@ if ( post_password_required() ) {
 	return;
 }
 
-$hero 				 = get_field( 'hero' );
-$descripcion 		 = get_field( 'descripcion' );
+$secciones = array(
+	'descripcion' => 'Descripción',
+	'padecimientos' => 'Padecimientos',
+	'procedimientos' => 'Procedimientos',
+	'informacion_general' => 'Información general',
+	// 'seccion_generica' => 'Sección genérica',
+);
+
 $padecimientos 		 = get_field( 'padecimientos' );
 $procedimientos 	 = get_field( 'procedimientos' );
 $informacion_general = get_field( 'informacion_general' );
@@ -48,33 +54,25 @@ $seccion_genericas 	 = get_field( 'seccion_generica' );
 		<div class="content-secc">
 			<div class="col-sec1 info-producto">
 				<h1><?php echo $product->name; ?></h1>
+
+				<?php if ( $mg_product->is_vendible() ) : ?>
+					<p class="price"><?php echo $product->get_price_html() ?></p>
+				<?php endif; ?>
+
+
+				<?php if ( have_rows( 'hero_instrucciones' ) ): ?>
+					<ul>
+						<?php while( have_rows( 'hero_instrucciones' ) ) : the_row(); ?>
+							<li><?php the_sub_field( 'texto' ); ?></li>
+						<?php endwhile; ?>
+					</ul>
+				<?php endif; ?>
+
+				<p><?php the_field( 'hero_texto' ) ?></p>
+
 				<?php 
-					if ( $mg_product->is_vendible() ) {
-						echo '<p class="price">' . $product->get_price_html() . '</p>';
-					}
-				?>
-				<ul>
-					<?php
-						if ($hero != null ){
-							$rows = $hero['instrucciones'];
-						
-							foreach( $rows as $row ) {
-							
-								echo '<li>';
-									echo $row['texto'];
-								echo '</li>';
-							}
-						}
-					?>
-				</ul>
-				<p><?php
-					if ($hero != null) {
-						echo $hero['texto'];
-					} 
-				?></p>
-				<?php 
-					if ( $mg_product->is_vendible() ) {
-						echo '<p>Cantidad</p>';
+					if ( $mg_product->is_vendible_without_agenda() ) {
+						// echo '<p>Cantidad</p>';
 						do_action('woocommerce_simple_add_to_cart');
 					}
 				?>
@@ -87,106 +85,57 @@ $seccion_genericas 	 = get_field( 'seccion_generica' );
 	</div>
 
 	<div class="seccion-submenu">
-		<?php
-			//echo $descripcion['mostrar'];
-			if ($descripcion != null) {
-				if ($descripcion['mostrar'] == true) {
-					if ($descripcion['titulo'] == null) {
-						echo '<div class="button">
-							<a href="#descripcion">Descripcion</a>
-						</div>';
-					}else {
-						echo '<div class="button">
-							<a href="#'. sanitize_title($descripcion['titulo']) .'">' . $descripcion['titulo'] . '</a>
-						</div>';
+		<?php foreach ( $secciones as $key => $label ) : ?>
+			<?php if ( have_rows( $key ) ) : ?>
+				<?php while( have_rows( $key) ): the_row();
+
+					if ( ! get_sub_field( 'mostrar' ) ) {
+						continue;
 					}
-					
-				}
-			}
 
-			if ($padecimientos != null) {
-				if ($padecimientos['mostrar'] == true) {
-					if ($padecimientos['titulo'] == null) {
-						echo '<div class="button">
-							<a href="#padecimientos">Padecimientos</a>
-						</div>';
-					}else {
-						echo '<div class="button">
-							<a href="#'. sanitize_title($padecimientos['titulo']) .'">' . $padecimientos['titulo'] . '</a>
-						</div>';
-					}
-				}
-			}
-
-			if ($procedimientos != null) {
-				if ($procedimientos['mostrar'] == true) {
-					if ($procedimientos['titulo'] == null) {
-						echo '<div class="button">
-							<a href="#procedimientos">Procedimientos</a>
-						</div>';
-					}else {
-						echo '<div class="button">
-							<a href="#'. sanitize_title($procedimientos['titulo']) .'">' . $procedimientos['titulo'] . '</a>
-						</div>';
-					}	
-				}
-			}
-
-			if ($informacion_general != null) {
-				if ($informacion_general['mostrar'] == true) {
-						echo '<div class="button">
-							<a href="#informacion-general">Informacion general</a>
-						</div>';
-				}
-			}
-
-			if ($seccion_genericas != null){
-				foreach( $seccion_genericas as $seccion_generica ) {
-					if ($seccion_generica['mostrar'] == true) {
-						echo '<div class="button">
-							<a href="#' . sanitize_title($seccion_generica['titulo']) . '">'. $seccion_generica['titulo'] .'</a>
-						</div>';
-					}
-				}
-			}
-			
-		?>
-		
-	</div>
-	
-	<?php
-		if ($descripcion != null) {
-			if ($descripcion['mostrar'] == true) {
-				if ($descripcion['titulo'] == null) {
-	?>
-	<div id="descripcion" class="seccion-des">
-		<div class="info-descripcion">
-			<h2>Descripcion</h2>
-			<?php echo $descripcion['texto']; ?>
-			
-		</div>
-		<div class="imagen-descripcion">
-			<?php echo wp_get_attachment_image($descripcion['imagen'], 'full'); ?>
-		</div>
-	</div>
-	<?php 
-				}else{
-	?>
-					<div id="<?php echo sanitize_title($descripcion['titulo']);?>" class="seccion-des">
-						<div class="info-descripcion">
-							<h2><?php echo $descripcion['titulo']; ?></h2>
-							<?php echo $descripcion['texto']; ?>
-							
-						</div>
-						<div class="imagen-descripcion">
-							<?php echo wp_get_attachment_image($descripcion['imagen'], 'full'); ?>
-						</div>
+					$id = get_sub_field( 'titulo' ) ? sanitize_title( get_sub_field( 'titulo' ) ) : "$key";
+				?>
+					<div class="button">
+						<a href="#<?php echo $id; ?>"><?php echo get_sub_field( 'titulo' ) ?: $label; ?></a>
 					</div>
-	<?php
-				}
+				<?php endwhile; ?>
+			<?php endif; ?>
+		<?php endforeach; ?>
+
+		<?php if ( have_rows( 'seccion_generica' ) ) : ?>
+			<?php while( have_rows( 'seccion_generica' ) ): the_row(); ?>
+				<?php if ( get_sub_field( 'mostrar' ) ) : ?>
+					<div class="button">
+						<a href="#<?php sanitize_title( the_sub_field( 'titulo' ) ); ?>"><?php the_sub_field( 'titulo' ); ?></a>
+					</div>
+				<?php endif; ?>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+
+	<!-- Secciones content -->
+
+	<?php if ( have_rows( 'descripcion' ) ) : ?>
+		<?php while( have_rows( 'descripcion') ): the_row(); 
+			if ( ! get_sub_field( 'mostrar' ) ) {
+				continue;
 			}
-		} 
-	?>
+
+			$id = get_sub_field( 'titulo' ) ? sanitize_title( get_sub_field( 'titulo' ) ) : 'descripcion';
+		?>
+		<div id="<?php echo $id; ?>" class="seccion-des">
+			<div class="info-descripcion">
+				<h2><?php echo get_sub_field( 'titulo' ) ?: 'Descripción'; ?></h2>
+				<?php the_sub_field( 'texto' ); ?>
+			</div>
+
+			<?php ?> 
+			<div class="imagen-descripcion">
+				<?php echo wp_get_attachment_image( the_sub_field( 'imagen' ) , 'full'); ?>
+			</div>
+		</div>
+		<?php endwhile; ?>
+	<?php endif; ?>
 
 	<div class="contect-seccion">
 		<?php 
