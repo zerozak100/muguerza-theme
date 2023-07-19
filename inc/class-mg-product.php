3fcf205d04c6198eaa4a37f006d63017c4da404b
Jeus Jurado <ijurado@acsyt.com>
2023-07-19 01:12:32 -0600

    classed disabled

--- inc/class-mg-product.php
+++ inc/class-mg-product.php
@@ -1,27 +1,27 @@
 <?php
 
-class MG_Product extends WC_Product_Simple {
-    public function __construct( $product = 0 ) {
-        parent::__construct( $product );
-    }
+// class MG_Product extends WC_Product_Simple {
+//     public function __construct( $product = 0 ) {
+//         parent::__construct( $product );
+//     }
 
-    public function is_especialidad() {
-        $is_especialidad = false;
+//     public function is_especialidad() {
+//         $is_especialidad = false;
 
-        $terms = get_the_terms( $this->get_id(), 'producto_tipo' );
+//         $terms = get_the_terms( $this->get_id(), 'producto_tipo' );
 
-        if ( ! empty( $terms ) ) {
-            $is_especialidad = 'especialidad' === $terms[0]->slug;
-        }
+//         if ( ! empty( $terms ) ) {
+//             $is_especialidad = 'especialidad' === $terms[0]->slug;
+//         }
 
-        return $is_especialidad;
-    }
+//         return $is_especialidad;
+//     }
 
-    public function is_vendible() {
-        return get_field( 'vendible', $this->get_id() );
-    }
+//     public function is_vendible() {
+//         return get_field( 'vendible', $this->get_id() );
+//     }
 
-    public function is_agendable() {
-        return get_field( 'agendable', $this->get_id() );
-    }
-}
+//     public function is_agendable() {
+//         return get_field( 'agendable', $this->get_id() );
+//     }
+// }
