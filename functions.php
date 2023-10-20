05a69f28c8dee2d54bc3d139e8a09d63a39a81d4
Jeus Jurado <ijurado@acsyt.com>
2023-10-20 13:19:48 -0600

    v1.0.0

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
  define( 'MG_TEMPLATES_PATH', get_stylesheet_directory() . '/templates//' );
- define( 'MG_THEME_VERSION', '0.0.1' );
+ define( 'MG_THEME_VERSION', '1.0.0' );
 
 /**
  * Load child theme css and optional scripts
