d0e0ec89b791ee31a8cc79b25f638a091b75efa6
Jeus Jurado <ijurado@acsyt.com>
2023-11-28 14:06:37 -0600

    fix: adjust styles

--- templates/home/sala-prensa-item.php
+++ templates/home/sala-prensa-item.php
@@ -1,22 +1,22 @@
 <?php
-
 $_classes = array_merge( array( 'home-sala-prensa-item' ), $classes );
 $_classes = implode( " ", $_classes );
-
-$image = wp_get_attachment_image( get_post_thumbnail_id( $post->ID ) );
+$image = wp_get_attachment_image( get_post_thumbnail_id( $post->ID ), 'full' );
 $title = $post->post_title;
 $excerpt = wp_trim_words( get_the_excerpt( $post->ID ), 15 );
 
 ?>
-
+​
 <div id="home-sala-prensa-item-<?php echo esc_attr( $post->ID ); ?>" class="<?php echo esc_attr( $_classes ); ?>">
-    <div class="home-sala-prensa-item__wrapper">
-        <figure class="home-sala-prensa-item__img-box">
-            <?php echo $image; ?>
-        </figure>
-        <div class="home-sala-prensa-item__content-box">
-            <h6 class="home-sala-prensa-item__title"><?php echo esc_html( $title ); ?></h6>
-            <p class="home-sala-prensa-item__excerpt"><?php echo esc_html( $excerpt ); ?></p>
+    <a href="<?php echo get_permalink($post->ID); ?>">
+        <div class="home-sala-prensa-item__wrapper">
+            <figure class="home-sala-prensa-item__img-box">
+                <?php echo $image; ?>
+            </figure>
+            <div class="home-sala-prensa-item__content-box">
+                <h6 class="home-sala-prensa-item__title"><?php echo esc_html( $title ); ?></h6>
+                <p class="home-sala-prensa-item__excerpt"><?php echo esc_html( $excerpt ); ?></p>
+            </div>
         </div>
-    </div>
+    </a>
 </div>
