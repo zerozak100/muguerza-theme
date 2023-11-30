a6cc396ab5b9f9688eb4eb57bf6c5a4f2c867198
Jeus Jurado <ijurado@acsyt.com>
2023-11-30 12:56:29 -0600

    feat: add see more link

--- templates/home/sala-prensa-item.php
+++ templates/home/sala-prensa-item.php
@@ -1,12 +1,14 @@
 <?php
+
 $_classes = array_merge( array( 'home-sala-prensa-item' ), $classes );
 $_classes = implode( " ", $_classes );
+
 $image = wp_get_attachment_image( get_post_thumbnail_id( $post->ID ), 'full' );
-$title = $post->post_title;
+$title = substr( $post->post_title, 0, 72 );
 $excerpt = wp_trim_words( get_the_excerpt( $post->ID ), 15 );
 
 ?>
-​
+
 <div id="home-sala-prensa-item-<?php echo esc_attr( $post->ID ); ?>" class="<?php echo esc_attr( $_classes ); ?>">
     <a href="<?php echo get_permalink($post->ID); ?>">
         <div class="home-sala-prensa-item__wrapper">
@@ -16,6 +18,7 @@ $excerpt = wp_trim_words( get_the_excerpt( $post->ID ), 15 );
             <div class="home-sala-prensa-item__content-box">
                 <h6 class="home-sala-prensa-item__title"><?php echo esc_html( $title ); ?></h6>
                 <p class="home-sala-prensa-item__excerpt"><?php echo esc_html( $excerpt ); ?></p>
+                <a href="<?php echo get_permalink($post->ID); ?>">Ver más</a>
             </div>
         </div>
     </a>
