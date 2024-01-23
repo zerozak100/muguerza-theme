babd726d128a22e4f2f64624929dcecd60de5707
Jeus Jurado <ijurado@acsyt.com>
2024-01-23 09:04:35 -0600

    v2.0.2

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
  define( 'MG_TEMPLATES_PATH', get_stylesheet_directory() . '/templates//' );
- define( 'MG_THEME_VERSION', '2.0.1' );
+ define( 'MG_THEME_VERSION', '2.0.2' );
 
 /**
  * Load child theme css and optional scripts
